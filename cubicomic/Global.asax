8f4f5b2e07d56d7f9e43c92a5e5de7468cd41eda
k4nnon <maxpayne_ga@hotmail.com>
2016-10-02 04:07:53 -0300

    Commit inicial del proyecto
    Proyecto nuevo de Visual Studio

---
+++ cubicomic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cubicomic.MvcApplication" Language="C#" %>
---
+++ cubicomic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cubicomic.Startup))]
+namespace cubicomic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
