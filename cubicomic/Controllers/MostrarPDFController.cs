c0b243276327bf467eeff9262967929248e97904
k4nnon <maxpayne_ga@hotmail.com>
2016-11-12 20:02:10 -0300

    Lector PDF
    funcional, falta asginarle que url leer, pero por defecto lee los pdf de
    uploads

---
+++ cubicomic/Controllers/MostrarPDFController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace cubicomic.Controllers
+{
+    public class MostrarPDFController : Controller
+    {
+        // GET: MostrarPDF
+        public ActionResult Index()
+        {
+
+            List<string> PDF = new List<string>();
+            var carpeta = Server.MapPath("~") + @"Uploads";
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo d = new DirectoryInfo(carpeta);
+            //Obtenemos todos los .jpg
+            FileInfo[] Files = d.GetFiles("*.PDF");
+            //Recorremos la carpeta
+            foreach (FileInfo file in Files)
+            {
+                PDF.Add(file.Name);
+            }
+            ViewBag.PDF = PDF;
+            return View();
+        }
+    }
+}
