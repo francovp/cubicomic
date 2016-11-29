170e9acaa92ceab5f920dc7ebbab8025de442887
k4nnon <maxpayne_ga@hotmail.com>
2016-11-29 17:11:17 -0300

    Visualisador de comic y mangas

---
+++ cubicomic/Controllers/LectorComicController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Web.Mvc;
+
+namespace cubicomic.Controllers
+{
+    public class LectorComicController : Controller
+    {
+        // GET: LectorComic
+        public ActionResult Index(String busqueda)
+        {
+            String nuevo = null;
+            ViewBag.Ruta = busqueda;
+            ArrayList comic = new ArrayList();
+            var carpeta = Server.MapPath("~") + @"UploadsComic/" + busqueda;
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo d = new DirectoryInfo(carpeta);
+            FileInfo[] Files = d.GetFiles("*.*");
+            //Recorremos la carpeta
+            foreach (FileInfo file in Files)
+            {
+                 comic.Add(file.Name);
+            }
+            ViewBag.comic = comic;
+            return View();
+        }
+
+        
+    }
+}
