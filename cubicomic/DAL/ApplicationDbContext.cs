2d74fc4af16f37e204d08df3678bdba4775952d8
Franco Valerio <franco.gvp@hotmail.com>
2016-11-30 03:24:37 -0300

    Probando conexión de base de datos de Azure "CubicomicTest"

--- cubicomic/DAL/ApplicationDbContext.cs
+++ cubicomic/DAL/ApplicationDbContext.cs
@@ -9,8 +9,8 @@ namespace cubicomic.DAL
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-           // : base("TestConnection", throwIfV1Schema: false)
-           : base("LocalConnection", throwIfV1Schema: false)
+            : base("TestConnection", throwIfV1Schema: false)
+           //: base("LocalConnection", throwIfV1Schema: false)
         {
         }
         public DbSet<File> Files { get; set; }
