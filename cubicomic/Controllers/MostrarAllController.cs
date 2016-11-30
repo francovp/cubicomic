250cef29882ef8b18960191c1918d0f7d812e122
Franco Valerio <franco.gvp@hotmail.com>
2016-11-29 21:13:45 -0300

    Arreglados Filtros de Vista MostrarAll

--- cubicomic/Controllers/MostrarAllController.cs
+++ cubicomic/Controllers/MostrarAllController.cs
@@ -31,7 +31,7 @@ namespace cubicomic.Controllers
             }
             ViewBag.lista = listaRutaImagenes;
 
-            //Busca PDF
+            //busca PDF
             List<string> listaRutaPDF = new List<string>();
             var carpetaPDF = Server.MapPath(uploadsPath);
             //Necesitas: using System.IO; para realizar esto
@@ -60,10 +60,25 @@ namespace cubicomic.Controllers
                 listaRutaComic.Add(file.Name);
             }
             ViewBag.muestraComic = listaRutaComic;
+
+            //buscar comic
+            List<string> listaRutaManga = new List<string>();
+            var carpetaMangas = Server.MapPath(uploadsPath);
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo decc = new DirectoryInfo(carpetaMangas);
+            //Obtenemos todos los .jpg con pdf adjunto
+            FileInfo[] Files4 = decc.GetFiles("*-manga-*");
+            //Recorremos la carpeta
+
+            foreach (FileInfo file in Files4)
+            {
+                listaRutaManga.Add(file.Name);
+            }
+            ViewBag.muestraManga = listaRutaManga;
             return View();
         }
 
-        public PartialViewResult _ImagenesAllPartial()
+        public PartialViewResult _AllPartial()
         {
             //busca imagenes
             List<string> listaRutaImagenes = new List<string>();
@@ -141,7 +156,24 @@ namespace cubicomic.Controllers
             {
                 listaRutaImagenes.Add(file.Name);
             }
-            ViewBag.lista = listaRutaImagenes;
+            ViewBag.listaImagenPartial = listaRutaImagenes;
+            return PartialView();
+        }
+
+        public PartialViewResult _ImagenesAllPartial(string id)
+        {
+            List<string> listaRutaImagenes = new List<string>();
+            var carpeta = Server.MapPath(uploadsPath);
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo da = new DirectoryInfo(carpeta);
+            //Obtenemos todos los .jpg
+            FileInfo[] Files = da.GetFiles("*.--image*");
+            //Recorremos la carpeta
+            foreach (FileInfo file in Files)
+            {
+                listaRutaImagenes.Add(file.Name);
+            }
+            ViewBag.listaImagenAll = listaRutaImagenes;
             return PartialView();
         }
 
@@ -152,14 +184,32 @@ namespace cubicomic.Controllers
             //Necesitas: using System.IO; para realizar esto
             DirectoryInfo de = new DirectoryInfo(carpetaPDF);
             //Obtenemos todos los .jpg con pdf adjunto
-            FileInfo[] Files = de.GetFiles("*pdf*");
+            FileInfo[] Files = de.GetFiles("*--." + @id + ".--pdf*");
             //Recorremos la carpeta
 
             foreach (FileInfo file in Files)
             {
                 listaRutaPDF.Add(file.Name);
             }
-            ViewBag.muestraPDF = listaRutaPDF;
+            ViewBag.listaNovelaPartial = listaRutaPDF;
+            return PartialView();
+        }
+
+        public PartialViewResult _NovelasAllPartial(string id)
+        {
+            List<string> listaRutaPDF = new List<string>();
+            var carpetaPDF = Server.MapPath(uploadsPath);
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo de = new DirectoryInfo(carpetaPDF);
+            //Obtenemos todos los .jpg con pdf adjunto
+            FileInfo[] Files = de.GetFiles("*.--pdf*");
+            //Recorremos la carpeta
+
+            foreach (FileInfo file in Files)
+            {
+                listaRutaPDF.Add(file.Name);
+            }
+            ViewBag.listaNovelaAll = listaRutaPDF;
             return PartialView();
         }
 
@@ -170,14 +220,32 @@ namespace cubicomic.Controllers
             //Necesitas: using System.IO; para realizar esto
             DirectoryInfo dec = new DirectoryInfo(carpetaComic);
             //Obtenemos todos los .jpg con pdf adjunto
-            FileInfo[] Files = dec.GetFiles("*-comic-*");
+            FileInfo[] Files = dec.GetFiles("*--." + @id + ".---comic*");
             //Recorremos la carpeta
 
             foreach (FileInfo file in Files)
             {
                 listaRutaComic.Add(file.Name);
             }
-            ViewBag.muestraComic = listaRutaComic;
+            ViewBag.listaComicPartial = listaRutaComic;
+            return PartialView();
+        }
+
+        public PartialViewResult _ComicsAllPartial(string id)
+        {
+            List<string> listaRutaComic = new List<string>();
+            var carpetaComic = Server.MapPath(uploadsPath);
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo dec = new DirectoryInfo(carpetaComic);
+            //Obtenemos todos los .jpg con pdf adjunto
+            FileInfo[] Files = dec.GetFiles("*.---comic*");
+            //Recorremos la carpeta
+
+            foreach (FileInfo file in Files)
+            {
+                listaRutaComic.Add(file.Name);
+            }
+            ViewBag.listaComicAll = listaRutaComic;
             return PartialView();
         }
 
@@ -188,14 +256,32 @@ namespace cubicomic.Controllers
             //Necesitas: using System.IO; para realizar esto
             DirectoryInfo dec = new DirectoryInfo(carpetaMangas);
             //Obtenemos todos los .jpg con pdf adjunto
-            FileInfo[] Files = dec.GetFiles("*-manga-*");
+            FileInfo[] Files = dec.GetFiles("*--." + @id + ".---manga*");
             //Recorremos la carpeta
 
             foreach (FileInfo file in Files)
             {
                 listaRutaManga.Add(file.Name);
             }
-            ViewBag.muestraManga = listaRutaManga;
+            ViewBag.listaMangaPartial = listaRutaManga;
+            return PartialView();
+        }
+
+        public PartialViewResult _MangasAllPartial(string id)
+        {
+            List<string> listaRutaManga = new List<string>();
+            var carpetaMangas = Server.MapPath(uploadsPath);
+            //Necesitas: using System.IO; para realizar esto
+            DirectoryInfo dec = new DirectoryInfo(carpetaMangas);
+            //Obtenemos todos los .jpg con pdf adjunto
+            FileInfo[] Files = dec.GetFiles("*.---manga*");
+            //Recorremos la carpeta
+
+            foreach (FileInfo file in Files)
+            {
+                listaRutaManga.Add(file.Name);
+            }
+            ViewBag.listaMangaAll = listaRutaManga;
             return PartialView();
         }
     }
